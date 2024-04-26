d4948b1f179cb07107a971b7fcd433e71fcb86e1
Anwender <kujawadaniel81@gmail.com>
2024-04-26 18:02:17 +0200

    first push

--- src/ifelse.java
+++ src/ifelse.java
@@ -1,13 +1,43 @@
+import java.util.Scanner;
+
 public class ifelse {
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
 
-        int x =65;
 
-        if (x ==65){
-            System.out.println("lol");
+        int punkte;
+        int zusatzp;
+        String name;
+
+        System.out.println("Name der Person : ");
+        Scanner scan = new Scanner(System.in);
+        name = scan.next();
+
+        System.out.println("Anzahl der Punkte von Person " + name );
+        Scanner scan2 = new Scanner(System.in);
+        punkte = scan2.nextInt();
+
+        System.out.println("Anzahl der Zusatzpunkte von " + name );
+        Scanner scan3 = new Scanner(System.in);
+        zusatzp = scan3.nextInt();
+
+
+        //punkte und zusatzpunkte addieren
+        int g = punkte + zusatzp;
+
+        //wenn name einen Punktewert von mindestens 50 hat, wird die verschachtelte Schleife gestartet
+        //ansonsten else
+        if (g >= 50 ){
+            // wenn Punktewert gleich 50 ist führe aus Ausgabe mit "gerade so bestanden aus", sonst else
+            if (g==50){
+                System.out.println(name + " hat " + punkte + " Punkte und " + zusatzp + " Zusatzpunkten die Prüfung gerade so bestanden.");
+            } else {
+                System.out.println(name + " hat " + punkte + " Punkten und " + zusatzp + " Zusatzpunkte bekommen, womit sie die Prüfung bestanden hat.");
+
+            }}
+        else {
+            System.out.println(name + " hat " + punkte + " Punkte und " + zusatzp +" Zusatzpunkte erhalten und die Prüfung leider nicht bestanden." );
         }
 
     }
-
 }
